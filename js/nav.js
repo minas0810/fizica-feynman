2cb2504f14c8b8056d1790044e47bddfec50f0e9
minas0810 <minas0810@gmail.com>
2026-05-20 22:06:38 +0300

    prof-minas

--- js/nav.js
+++ js/nav.js
@@ -62,7 +62,7 @@
   function aboutHref()        { return isTopLevel ? 'despre-feynman.html' : '../despre-feynman.html'; }
   function modulesHref()      { return isTopLevel ? 'modules.html'        : '../modules.html'; }
   function simulariHref()     { return isTopLevel ? 'simulari.html'       : '../simulari.html'; }
-  function authorHref()       { return 'https://mate-fizica.eu'; }
+  function authorHref()       { return 'https://prof-minas.ro'; }
 
   /* ================================================
      1. NAVBAR GLOBALĂ
